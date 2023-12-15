aca54014eb369ade243ef11a8ebf2382d343bf8f
Nakkasenp65 <nakkasenputtacharoen@gmail.com>
2023-12-16 01:16:26 +0700

    -Add GUI and Firebase Realtime Database for student registration -Implement student registration with GUI and Firebase Realtime Database

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ import tkinter as tk
 import util
 import cv2
 from PIL import Image, ImageTk
+import customtkinter as ctk
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 
 class App:
+
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://faceattendacerealtime-ffcf8-default-rtdb.firebaseio.com/"
+    })
+
+    ref = db.reference('Students')
+    year = 2023
+    student_id = 1
+
     def __init__(self):
         self.main_window = tk.Tk()
         self.main_window.geometry("1200x520+350+100")
@@ -54,26 +69,57 @@ class App:
         pass
 
     def register_new_user(self):
-        self.register_new_user_window = tk.Toplevel(self.main_window)
+        # self.register_new_user_window = tk.Toplevel(self.main_window)
+        self.register_new_user_window = ctk.CTkToplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")
         self.register_new_user_window.resizable(False, False)
+        self.register_new_user_window.title("Register New User")
+        self.register_new_user_window.configure(bg='white')
 
-        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
-        self.accept_button_register_new_user_window.place(x=750, y=300)
+        '#accept button'
+        # self.accept_button_register_new_user_window = util.get_ctk_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window = util.get_ctk_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window.place(x=750, y=345)
 
-        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try Again', 'red', self.try_again_register_new_user)
-        self.try_again_button_register_new_user_window.place(x=750, y=400)
+        '#try again button'
+        # self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try Again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window = util.get_ctk_button(self.register_new_user_window, 'Try Again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window.place(x=930, y=345)
 
         self.capture_label = util.get_img_label(self.register_new_user_window)
         self.capture_label.place(x=10, y=0, width=700, height=500)
 
         self.add_img_to_label(self.capture_label)
 
-        self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
-        self.entry_text_register_new_user.place(x=750, y=150)
+        # self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+        # self.entry_text_register_new_user.place(x=750, y=150)
+        #
+        # self.name_entry_text_register_new_user = util.get_entry_name(self.register_new_user_window)
+        # self.name_entry_text_register_new_user.place(x=750, y=60)
+        #
+        # self.name_entry_label_register_new_user = util.get_text_label(self.register_new_user_window, 'name:')
+        # self.name_entry_label_register_new_user.place(x=750, y=40)
 
-        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Enter your name:')
-        self.text_label_register_new_user.place(x=750, y=70)
+        # Name Field
+        self.name_entry_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, 'Full name:')
+        self.name_entry_label_register_new_user.place(x=750, y=40)
+
+        self.name_entry_text_register_new_user = util.get_entry_input(self.register_new_user_window, 'Enter your name')
+        self.name_entry_text_register_new_user.place(x=750, y=80)
+
+        # Faculty Field
+        self.faculty_entry_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, 'Faculty:')
+        self.faculty_entry_label_register_new_user.place(x=750, y=140)
+
+        self.faculty_entry_text_register_new_user = util.get_entry_input(self.register_new_user_window, 'Enter your faculty')
+        self.faculty_entry_text_register_new_user.place(x=750, y=180)
+
+        # Starting Year Field
+        self.starting_year_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, text='Starting Year:')
+        self.starting_year_label_register_new_user.place(x=750, y=240)
+
+        self.starting_year_entry_text_register_new_user = util.get_entry_input(self.register_new_user_window, 'Enter your starting year')
+        self.starting_year_entry_text_register_new_user.place(x=750, y=280)
 
     def try_again_register_new_user(self):
         self.register_new_user_window.destroy()
@@ -88,10 +134,29 @@ class App:
     def start(self):
         self.main_window.mainloop()
 
-    def accept_register_new_user(self):
-        name = self.entry_text_register_new_user.get(1.0, 'end-1c')
+    def add_to_db(self,name, faculty, starting_year, student_id):
+        ref= db.reference('Students')
+        data = {
+            'name': name,
+            'faculty': faculty,
+            'starting_year': starting_year,
+            'year': 2023 - int(starting_year),
+            'total_attendance': 0,
+            'last_attendance': 'Never'
+        }
+        ref.child(str(student_id)).set(data)
 
-        cv2.imwrite(os.path.join(self.db_dir, '{}'))
+    def accept_register_new_user(self):
+
+        ref = db.reference('Students')
+        name = self.name_entry_text_register_new_user.get()
+        faculty = self.faculty_entry_text_register_new_user.get()
+        starting_year = self.starting_year_entry_text_register_new_user.get()
+        if not name or not faculty or not starting_year:
+            print("Please fill in all fields.")
+            return
+        self.add_to_db(name, faculty, starting_year, self.student_id)
+        self.student_id += 1
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -5,6 +5,8 @@ import tkinter as tk
 from tkinter import messagebox
 import face_recognition
 
+import customtkinter as ctk
+
 
 def get_button(window, text, color, command, fg='white'):
     button = tk.Button(
@@ -23,24 +25,52 @@ def get_button(window, text, color, command, fg='white'):
     return button
 
 
+def get_ctk_button(window, text, color, command):
+    button = ctk.CTkButton(master=window,
+                           width=120,
+                           height=35,
+                           text=text,
+                           command=command,
+                           fg_color='black',
+                           corner_radius=20,
+                           font=('Century Gothic', 16)
+                           )
+    return button
+
+
 def get_img_label(window):
     label = tk.Label(window)
     label.grid(row=0, column=0)
     return label
 
 
+def get_img_label_with_text(window, text):
+    label = ctk.CTkLabel(window, text=text)
+
+
 def get_text_label(window, text):
     label = tk.Label(window, text=text)
-    label.config(font=("sans-serif", 21), justify="left")
+    label.config(font=("sans-serif", 16), justify="left")
     return label
 
 
 def get_entry_text(window):
-    inputtxt = tk.Text(window,
-                       height=2,
-                       width=15, font=("Arial", 32))
+    inputtxt = tk.Text(window, height=0.5, width=15, font=("Arial", 32))
     return inputtxt
 
+
+def get_entry_input(window, placeholder_text):
+    inputtxt = ctk.CTkEntry(window, placeholder_text=placeholder_text, height=50, width=300, font=("Arial", 16))
+    return inputtxt
+
+
+def get_text_ctk_label(window, text):
+    label = ctk.CTkLabel(window,
+                         text=text,
+                         font=("Century Gothic", 16),
+                         text_color="black",
+                         )
+    return label
 
 def msg_box(title, description):
     messagebox.showinfo(title, description)
