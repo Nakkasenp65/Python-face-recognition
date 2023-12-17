aed3b9e1ba51dd9281070f788c7af03042acc0b4
NP_long <117569395+Nakkasenp65@users.noreply.github.com>
2023-12-17 18:01:01 +0700

    -Add sync to database

---
+++ main.py
@@ -0,0 +1,352 @@
+import datetime
+import os.path
+import subprocess
+import tkinter as tk
+import util
+import cv2
+import numpy as np
+from PIL import Image, ImageTk
+
+import face_recognition
+import customtkinter as ctk
+from customtkinter import CTkImage
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+
+
+class App:
+
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://faceattendacerealtime-ffcf8-default-rtdb.firebaseio.com/",
+        'storageBucket': "faceattendacerealtime-ffcf8.appspot.com"
+    })
+
+   
+
+    def __init__(self):
+        
+        self.db_dir = './db'
+        total_known_faces = 0
+        for path in os.listdir(self.db_dir):
+            if path.endswith('.jpg'):
+                total_known_faces += 1
+        print('Total local registered face: ',total_known_faces)
+
+        self.bucket = storage.bucket()
+        self.ref = db.reference('Students')
+        
+        self.total_student_object = self.ref.get()
+        if self.total_student_object is None:
+            self.student_id = 0
+        else:
+            self.student_id = len(self.ref.get())-1
+        print('Total firebase registered student: ', self.student_id)
+
+        if total_known_faces != self.student_id:
+            print('Syncing database images with localdb...')
+            self.sync_database_images_with_localdb()
+            print('Syncing completed.')
+        else: 
+            print('Database images and localdb are synced.')
+            print('Starting app...')
+
+        self.main_window = tk.Tk()
+        self.main_window.geometry("960x500+350+100")
+        self.main_window.title("Face Recognition")
+
+        self.show_total_students_label = util.get_text_ctk_label(self.main_window, 'Total students: ', 16, 'black')
+        self.show_total_students_label.place(x=720, y=70)
+
+        self.show_total_students_label = util.get_text_ctk_label(self.main_window, str(self.student_id), 16, 'black')
+        self.show_total_students_label.place(x=850, y=70)
+
+        self.title_text_main_window = util.get_text_ctk_label(self.main_window, "Face Recognition", 24, "black")
+        self.title_text_main_window.place(x=720, y=20)
+
+        self.login_button_main_window = util.get_ctk_button(self.main_window, "Login", "black", self.login )
+        self.login_button_main_window.place(x=820, y=325)
+
+        self.register_button_main_window = util.get_ctk_button(self.main_window, "Register", "black", self.register_new_user )
+        self.register_button_main_window.place(x=820, y=385)
+
+        self.show_profile_button_new_user_window = util.get_ctk_button(self.main_window, 'Profile','blue', self.show_profile)
+        self.show_profile_button_new_user_window.place(x=820, y=445)
+
+        self.webcam_label = util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_webcam(self.webcam_label)
+
+        if not os.path.exists(self.db_dir):
+            os.mkdir(self.db_dir)
+
+    def sync_database_images_with_localdb(self):
+        # total_student = self.student_id
+        for i in range(1, self.student_id+1):
+            print(i, '.jpg sync')
+            blob = self.bucket.get_blob(f'Images/{i}.jpg')
+            array = np.frombuffer(blob.download_as_string(), np.uint8)
+            imgStudent = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+            cv2.imwrite(os.path.join(self.db_dir, str(i) + '.jpg'), imgStudent)
+
+
+    def add_webcam(self, label):
+        if 'cap' not in self.__dict__:
+            self.cap = cv2.VideoCapture(0)
+
+        self._label = label
+
+        self.process_webcam()
+
+    def process_webcam(self):
+
+        ret, frame = self.cap.read()
+        self.most_recent_capture_arr = frame
+        img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
+        self.most_recent_capture_pil = Image.fromarray(img_)
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+
+        self._label.imgtk = imgtk
+        self._label.configure(image=imgtk)
+
+        self._label.after(20, self.process_webcam)
+
+    def login(self):
+        unknown_img_path = './.tmp.jpg'
+
+        cv2.imwrite(unknown_img_path, self.most_recent_capture_arr)
+
+        output = str(subprocess.check_output(['face_recognition', self.db_dir, unknown_img_path]))
+        print(output)
+        static_student_id = output.split(',')[1][:-5]
+        print(static_student_id)
+
+        try:
+            check_id = int(static_student_id)
+            os.remove(unknown_img_path)
+        except ValueError:
+            util.show_error("No match found.")
+            os.remove(unknown_img_path)
+            return
+
+        student_already_attended = self.ref.child(static_student_id).get()['last_attendance']
+        student_already_attended = student_already_attended.split(' ')[0]
+        if  student_already_attended == datetime.datetime.now().strftime("%d/%m/%Y"):
+            util.show_checkmark("You already attended today.")
+            return
+        else :
+            student_name = self.ref.child(static_student_id).get()['name']
+            student_name = student_name.split(' ')[0]
+
+            total_attendance = self.ref.child(static_student_id).get()['total_attendance']
+            total_attendance += 1
+
+            last_attendance = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+            self.ref.child(static_student_id).update({'last_attendance': last_attendance,
+                                                      'total_attendance': total_attendance})
+            print('Student ID: ', static_student_id)
+            notification = "Hello again, "+student_name+"\nDate: "+last_attendance
+            util.show_checkmark(notification)
+
+    def show_profile(self):
+        unknown_img_path = './.tmp.jpg'
+
+        cv2.imwrite(unknown_img_path, self.most_recent_capture_arr)
+
+        output = str(subprocess.check_output(['face_recognition', self.db_dir, unknown_img_path]))
+        os.remove(unknown_img_path)
+        print(output)
+        static_student_id = output.split(',')[1][:-5]
+        print(static_student_id)
+
+        if static_student_id.__contains__("unknown_person"):
+            util.show_error("Sorry, you are not registered.\n\nTry registering first.")
+            return
+        elif static_student_id.__contains__("no_persons_found"):
+            util.show_error("Sorry, no face detected.\n\nPlease, Try again")
+            return
+        else:
+            pass
+
+        student_data = self.ref.child(static_student_id).get()
+        if student_data is None:
+            util.show_error("Error retrieving student information.")
+            return
+
+        student_name = student_data.get('name', 'N/A')
+        student_major = student_data.get('major', 'N/A')
+        student_starting_year = student_data.get('starting_year', 'N/A')
+        student_year = student_data.get('year', 'N/A')
+        student_total_attendance = student_data.get('total_attendance', 'N/A')
+        student_last_attendance = student_data.get('last_attendance', 'N/A')
+
+        student_information = (
+            "Student id: " + static_student_id
+            + "\nName: " + student_name
+            + "\nMajor: " + student_major
+            + "\nStarting year: " + student_starting_year
+            + "\nYear: " + str(student_year)
+            + "\nTotal attendance: " + str(student_total_attendance)
+            + "\nLast attendance: " + student_last_attendance
+        )
+
+        self.register_new_user_window = ctk.CTkToplevel(self.main_window)
+        self.register_new_user_window.geometry("1200x520+370+120")
+        self.register_new_user_window.resizable(False, False)
+        self.register_new_user_window.title("Register New User")
+        self.register_new_user_window.configure(bg='white')
+
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_profile_img_to_label(static_student_id, self.capture_label)
+
+        self.profile_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, 'Profile', 24, 'white')
+        self.profile_label_register_new_user.place(x=750, y=20)
+
+        self.information_label_register_new_user = util.get_information_text_ctk_label(self.register_new_user_window, student_information, 24, 'white')
+        self.information_label_register_new_user.place(x=750, y=60)
+
+    def register_new_user(self):
+
+        is_no_face = False
+        is_registered = False
+        unknown_img_path = './.tmp.jpg'
+
+        cv2.imwrite(unknown_img_path, self.most_recent_capture_arr)
+        output = str(subprocess.check_output(['face_recognition', self.db_dir, unknown_img_path]))
+        print(output)
+        os.remove(unknown_img_path)
+        if output.__contains__("unknown_person"):
+            is_registered = False
+        elif output.__contains__("no_persons_found"):
+            is_no_face = True
+        else:
+            is_registered = True
+
+        if is_registered:
+            print(is_registered)
+            util.show_error("You are already registered.")
+            return
+        elif is_no_face:
+            print(is_no_face)
+            util.show_error("Sorry, no face detected.\n\nPlease, Try again.")
+            return
+        else:
+
+
+            '#custom tkinter window'
+            self.register_new_user_window = ctk.CTkToplevel(self.main_window)
+            self.register_new_user_window.geometry("1200x520+370+120")
+            self.register_new_user_window.resizable(False, False)
+            self.register_new_user_window.title("Register New User")
+
+            '#accept button'
+            self.accept_button_register_new_user_window = util.get_ctk_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+            self.accept_button_register_new_user_window.place(x=750, y=450)
+
+            '#try again button'
+            self.try_again_button_register_new_user_window = util.get_ctk_button(self.register_new_user_window, 'Try Again', 'red', self.try_again_register_new_user)
+            self.try_again_button_register_new_user_window.place(x=930, y=450)
+
+            '#capture label'
+            self.capture_label = util.get_img_label(self.register_new_user_window)
+            self.capture_label.place(x=10, y=0, width=700, height=500)
+
+            self.add_img_to_label(self.capture_label)
+
+            '# Name Field'
+            self.name_entry_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, 'Full name:', 16, 'white')
+            self.name_entry_label_register_new_user.place(x=770, y=40)
+
+            self.name_entry_text_register_new_user = util.get_entry_input(self.register_new_user_window, 'Your name')
+            self.name_entry_text_register_new_user.place(x=750, y=70)
+
+            '# Faculty Field'
+            self.major_entry_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, 'Major:', 16, 'white')
+            self.major_entry_label_register_new_user.place(x=770, y=160)
+
+            self.major_entry_text_register_new_user = util.get_combobox(self.register_new_user_window)
+            self.major_entry_text_register_new_user.place(x=750, y=190)
+
+            '# Starting Year Field'
+            self.starting_year_label_register_new_user = util.get_text_ctk_label(self.register_new_user_window, 'Starting Year:', 16, 'white')
+            self.starting_year_label_register_new_user.place(x=770, y=280)
+
+            self.starting_year_entry_text_register_new_user = util.get_entry_input(self.register_new_user_window, 'Starting year')
+            self.starting_year_entry_text_register_new_user.place(x=750, y=310)
+
+
+
+    def try_again_register_new_user(self):
+        self.register_new_user_window.destroy()
+
+    def add_img_to_label(self, label):
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        label.imgtk = imgtk
+        label.configure(image=imgtk)
+
+        self.register_new_user_capture = self.most_recent_capture_arr.copy()
+
+    def add_profile_img_to_label(self, number_id, label):
+
+        profile_img = cv2.imread(os.path.join(self.db_dir, str(number_id) + '.jpg'))
+        img_ = cv2.cvtColor(profile_img, cv2.COLOR_BGR2RGB)
+        profile_img = Image.fromarray(img_)
+
+        imgtk = ImageTk.PhotoImage(image=profile_img)
+        label.imgtk = imgtk
+        label.configure(image=imgtk)
+
+    def start(self):
+        self.main_window.mainloop()
+
+    def add_to_db(self,name, major, starting_year, student_id):
+        ref= db.reference('Students')
+        data = {
+            'name': name,
+            'major': major,
+            'starting_year': starting_year,
+            'year': 2023 - int(starting_year),
+            'total_attendance': 0,
+            'last_attendance': 'Never'
+        }
+        ref.child(str(student_id)).set(data)
+
+    
+    def upload_image(self, fileName):
+        bucket = storage.bucket()
+        blob = bucket.blob(fileName)
+        blob.upload_from_filename(fileName)
+
+    def accept_register_new_user(self):
+
+        name = self.name_entry_text_register_new_user.get()
+        major = self.major_entry_text_register_new_user.get()
+        starting_year = self.starting_year_entry_text_register_new_user.get()
+
+        if not name or not major or not starting_year:
+            util.empty_fields(self.register_new_user_window)
+            print("Please fill in all fields.")
+            return
+
+        else:
+            self.student_id += 1
+            self.add_to_db(name, major, starting_year, self.student_id)
+            cv2.imwrite(os.path.join(self.db_dir, str(self.student_id) + '.jpg'), self.register_new_user_capture)
+            self.upload_image('./db/'+str(self.student_id)+'.jpg')
+            self.show_total_students_label.configure(text=str(self.student_id))
+            self.register_new_user_window.destroy()
+            util.show_checkmark("Registered success.")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
---
+++ util.py
@@ -0,0 +1,161 @@
+import os
+import pickle
+
+import tkinter as tk
+from tkinter import messagebox
+import face_recognition
+
+import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
+
+
+
+def get_button(window, text, color, command, fg='white'):
+    button = tk.Button(
+                        window,
+                        text=text,
+                        activebackground="black",
+                        activeforeground="white",
+                        fg=fg,
+                        bg=color,
+                        command=command,
+                        height=2,
+                        width=20,
+                        font=('Helvetica bold', 20)
+                    )
+    return button
+
+
+def get_ctk_button(window, text, color, command):
+    button = ctk.CTkButton(master=window,
+                           width=120,
+                           height=40,
+                           text=text,
+                           command=command,
+                           fg_color='black',
+                           corner_radius=50,
+                           font=('Century Gothic', 16)
+                           )
+    return button
+
+
+def get_img_label(window):
+    label = tk.Label(window)
+    label.grid(row=0, column=0)
+    return label
+
+
+def get_img_label_with_text(window, text):
+    label = ctk.CTkLabel(window, text=text)
+
+
+def get_text_label(window, text):
+    label = tk.Label(window, text=text)
+    label.config(font=("sans-serif", 16), justify="left")
+    return label
+
+
+def get_entry_text(window):
+    inputtxt = tk.Text(window, height=0.5, width=15, font=("Arial", 32))
+    return inputtxt
+
+
+'#custom tkinter input'
+
+
+def get_entry_input(window, placeholder_text):
+    inputtxt = ctk.CTkEntry(window, placeholder_text=placeholder_text, height=50, width=300, font=("Arial", 16),
+                            placeholder_text_color='#676767', corner_radius=50)
+    return inputtxt
+
+
+'#custom tkinter label'
+
+
+def get_information_text_ctk_entry(window, fontsize, text_color):
+    label = ctk.CTkEntry(window,
+                         font=("Century Gothic", fontsize),
+                         text_color=text_color,
+                         height=300,
+                         width=300,
+                         justify='left',
+                         state='disabled',
+                         corner_radius=50
+                         )
+    return label
+
+def get_information_text_ctk_label(window, text, fontsize, text_color):
+    label = ctk.CTkLabel(window,
+                         text=text,
+                         font=("Century Gothic", fontsize),
+                         text_color=text_color,
+                         height=300,
+                         width=300,
+                         justify='left',
+                     )
+    return label
+
+def get_text_ctk_label(window, text, fontsize, text_color):
+    if text.__contains__('-1'):
+        text = '0'
+    label = ctk.CTkLabel(window,
+                         text=text,
+                         font=("Century Gothic", fontsize),
+                         text_color=text_color,
+                         )
+    return label
+
+
+'#custom tkinter comboBox'
+def get_combobox(window):
+    values = ['Computer science','Computer Engineering', 'Communications', 'Engineering', 'Business']
+    combobox = ctk.CTkComboBox(window, values=values, width=300, height=50, font=("Arial", 16), corner_radius=50)
+    return combobox
+
+
+'#custom messagebox'
+
+
+def show_error(text_message):
+    CTkMessagebox(title="Error", message=text_message, icon="cancel")
+
+
+def show_checkmark(text_message):
+    CTkMessagebox(title="Notification", message=text_message,icon="check", option_1="Okay")
+
+def empty_fields(window):
+    name_entry_empty_label = get_text_ctk_label(window, 'Name is not correct.', 14, '#B00000')
+    name_entry_empty_label.place(x=750, y=120)
+    major_entry_empty_label = get_text_ctk_label(window, 'Major is not correct.', 14, '#B00000')
+    major_entry_empty_label.place(x=750, y=240)
+    starting_year_entry_empty_label = get_text_ctk_label(window, 'Starting year is not correct.', 14, '#B00000')
+    starting_year_entry_empty_label.place(x=750, y=360)
+
+
+
+def recognize(img, db_path):
+    # it is assumed there will be at most 1 match in the db
+
+    embeddings_unknown = face_recognition.face_encodings(img)
+    if len(embeddings_unknown) == 0:
+        return 'no_persons_found'
+    else:
+        embeddings_unknown = embeddings_unknown[0]
+
+    db_dir = sorted(os.listdir(db_path))
+
+    match = False
+    j = 0
+    while not match and j < len(db_dir):
+        path_ = os.path.join(db_path, db_dir[j])
+
+        file = open(path_, 'rb')
+        embeddings = pickle.load(file)
+
+        match = face_recognition.compare_faces([embeddings], embeddings_unknown)[0]
+        j += 1
+
+    if match:
+        return db_dir[j - 1][:-7]
+    else:
+        return 'unknown_person'
